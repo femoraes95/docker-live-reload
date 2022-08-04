bbe8102abedc4e1c71b3a8641e3452bd2fcc6fe0
felipemoraesm <felipemoraes.ti@outlook.com>
2022-08-04 10:57:59 -0300

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
 FROM node:17-slim
 
 RUN apt-get update\
-    && apt-get install -y build-essential
+    && apt-get install -y build-essential\
+    && apt-get install -y gconf-service libgbm-dev libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libnss3 lsb-release xdg-utils wget
 
 WORKDIR /app-reload/
 
 COPY package.json package-lock.json /app-reload/
 
 RUN npm i 
+
+RUN npm i -g puppeteer --unsafe-perm=true -allow-root
 
 COPY . .
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 const express = require('express');
-
+const venom = require('venom-bot');
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const fs = require('fs');
-const filename = `./src/logs/new.txt`;
+venom
+  .create({
+    session: 'migracao',
+    multidevice: true,
+  })
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
 
-fs.closeSync(fs.openSync(filename, 'w'));
+function start(client) {
+  client.onMessage((message) => {
+    if (message.isGroupMsg === false) {
+      client
+        .sendText(
+          message.from,
+          'Olá, \n\nTudo bem?\n\nSou a Paula, assistente virtual do Hospital Paulista.\n\nEstamos centralizando nosso atendimento no WhatsApp oficial do Hospital, segue contato. \n\nhttps://api.whatsapp.com/send?phone=551150878700'
+        )
+        .then(() => {})
+        .catch((erro) => {
+          console.error('Error when sending: ', erro);
+        });
+    }
+  });
+}
 
 module.exports = app;
